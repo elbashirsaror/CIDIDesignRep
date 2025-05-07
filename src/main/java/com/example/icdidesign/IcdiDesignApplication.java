f8fe6b875d195652f58e06502d21ddc11bd0cb44
Elbashir Saror <elbashir1993@hotmail.com>
2025-05-07 23:32:04 +0300

    Project is added to github repo

---
+++ src/main/java/com/example/icdidesign/IcdiDesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.icdidesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcdiDesignApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(IcdiDesignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/icdidesign/IcdiDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.icdidesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcdiDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
